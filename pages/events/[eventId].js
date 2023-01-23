f3097d9d0fb7368766217d916a3e9c3b39ee72b1
Linden Hamer <linden@mynethub.com>
2023-01-23 14:54:29 +0000

    updated all events page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,28 +1,22 @@
 //core imports
 import { Fragment } from "react"
-import { useRouter } from "next/router"
 // component imports
 import EventSummary from "../../components/event-detail/event-summary"
 import EventLogistics from "../../components/event-detail/event-logistics"
 import EventContent from "../../components/event-detail/event-content"
 //dummy data import
-import { getEventById } from "../../dummy-data"
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util"
 import ErrorAlert from "../../components/ui/error-alert"
 
 // define function
-function EventDetailPage() {
-  // define router
-  const router = useRouter()
-  // router query to cgrab the event ID
-  const eventId = router.query.eventId
-  // run the function from dummy data
-  const event = getEventById(eventId)
+function EventDetailPage(props) {
+  const event = props.selectedEvent
   // check for NOT truthy
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     )
   }
 
@@ -40,6 +34,30 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   )
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId
+
+  const event = await getEventById(eventId)
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+    revalidate: 30,
+  }
+}
+
+export async function getStaticPaths() {
+  const events = await getFeaturedEvents()
+
+  const paths = events.map((event) => ({ params: { eventId: event.id } }))
+
+  return {
+    paths: paths,
+    fallback: "blocking",
+  }
 }
 
 export default EventDetailPage
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,13 @@
 import { Fragment } from "React"
 import { useRouter } from "next/router"
-
-import { getAllEvents } from "../../dummy-data"
+import { getAllEvents } from "../../helpers/api-util"
 import EventList from "../../components/event/event-list"
 import EventSearch from "../../components/event/event-search"
+import { getStaticProps } from ".."
 
-function AllEventsPage() {
-  const events = getAllEvents()
+function AllEventsPage(props) {
   const router = useRouter()
+  const { events } = props
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`
@@ -21,6 +21,17 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   )
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents()
+
+  return {
+    props: {
+      events: events,
+      revalidate: 60,
+    },
+  }
 }
 
 export default AllEventsPage
